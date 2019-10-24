43bb68262d3e602b3f88be9d5b68348579ff2f9f
Joey Nevarez <josephnevarez12@gmail.com>
2019-10-24 12:48:52 -0700

    added font awesome icons

--- src/components/NasaCard.js
+++ src/components/NasaCard.js
@@ -4,16 +4,14 @@ import Title from './Title';
 import Date from './Date';
 import Img from './Img';
 import Description from './Description';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-// import {fasRocket } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faRocket } from '@fortawesome/free-solid-svg-icons';
 
 
 const NasaCard = (props)=>{
     return (
         <NasaCardContainer>
-            {/* <FontAwesomeIcon icon="rocket" /> */}
-            {/* {<i class="fas fa-rocket"></i>} */}
-            <Title>{props.title}</Title>
+            <Title><FontAwesomeIcon icon={faRocket} />{props.title}<FontAwesomeIcon icon={faRocket} /></Title>
             <Date>{props.date}</Date>
             <Img src={props.url}/>
             <Description>{props.explanation}</Description>
