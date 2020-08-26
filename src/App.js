6620a872a53478f8f562e70dd7f2f0c6a593d143
Joey Nevarez <josephnevarez12@gmail.com>
2020-08-26 16:56:05 -0700

    fixes bug - see another date feature is functional

--- src/App.js
+++ src/App.js
@@ -4,21 +4,15 @@ import NasaCard from './components/NasaCard';
 import axios from 'axios';
 
 function App() {
-    // const [nasaData, setNasaData] = useEffect();
-    // const moment = require('moment');
-    // const [date, setDate] = useState(moment().format("YYYY-MM-DD"));
-
-    const [date, setDate] = useState(); 
+    const moment = require('moment');
+    const [date, setDate] = useState(moment().format("YYYY-MM-DD"));
     const [explanation, setExplanation] = useState();
     const [title, setTitle] = useState();
     const [image, setImage] = useState();
     const [copyright, setCopyright] = useState();
   
     useEffect(()=>{
-        // axios.get(`https://api.nasa.gov/planetary/apod?api_key=sfSJmkALfA0b755hFJUEmj9ck7OdUaVBFuni91bt`)
-        // axios.get(`https://api.nasa.gov/planetary/apod?api_key=sfSJmkALfA0b755hFJUEmj9ck7OdUaVBFuni91bt?date=${date}`)
-        axios.get('https://api.nasa.gov/planetary/apod?api_key=fa5O3BQljsEyqfNarlgF5bX4t4TbK6vLbySh9i7H&')        
-        // .then(res => setNasaData(res.data))
+        axios.get(`https://api.nasa.gov/planetary/apod?api_key=sfSJmkALfA0b755hFJUEmj9ck7OdUaVBFuni91bt&date=${date}`)
         .then(res => {
             setDate(res.data.date)
             setExplanation(res.data.explanation)
@@ -27,38 +21,28 @@ function App() {
             setCopyright(res.data.copyright)
         })
         .catch(err=> console.log(err))
-    // }, [date]);
-    }, []);
+    }, [date]);
     
-    // const selectDifferentDate = e => {
-    //     e.preventDefault();
-    //     let calender = e.target.value;
-    //     if(!moment(calender, 'YYYY-MM-DD').isValid()){
-    //         console.log('please enter correct date format')
-    //     } else {
-    //         setDate(calender);
-    //     }
-
-    // }
+    const selectDifferentDate = e => {
+        e.preventDefault();
+        let calender = e.target.value;
+        if(!moment(calender, 'YYYY-MM-DD').isValid()){
+            console.log('please enter correct date format')
+        } else {
+            setDate(calender);
+        }
+    }
 
     return (
         <div id="wrapper">
             <div id="container">
                 <NasaCard 
+                    selectDifferentDate={selectDifferentDate}
                     date={date}
                     explanation={explanation}
                     title={title}
                     image={image}
-                    copyright={copyright}
-                    // selectDifferentDate={selectDifferentDate}
-
-                    // date={nasaData.date}
-                    // explanation={nasaData.explanation}
-                    // title={nasaData.title}
-                    // image={nasaData.hdurl}
-                    // copyright={nasaData.copyright}
-                    // selectDifferentDate={selectDifferentDate}
-                    />
+                    copyright={copyright}/>
             </div>
         </div>
     );
